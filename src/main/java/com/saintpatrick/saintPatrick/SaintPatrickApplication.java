cf96dc80f006c0172971778a5f0bacbf4275dc73
Ramiro <ramiro.balsaa@gmail.com>
2025-02-24 23:33:32 -0300

    Primer commit

---
+++ src/main/java/com/saintpatrick/saintPatrick/SaintPatrickApplication.java
@@ -0,0 +1,13 @@
+package com.saintpatrick.saintPatrick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaintPatrickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaintPatrickApplication.class, args);
+	}
+
+}
